44535e12cfdcc11090afa00d0dea409d4d44b5aa
viho-kernel <vihari.reddy1802@gmail.com>
2025-12-19 15:25:43 +0530

    latest commmit

--- pokemon.sh
+++ pokemon.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for pokemon in $(curl -sL https://pokeapi.co/api/v2/pokemon?limit=10000 | jq '.results[].name' -r)
+ALL_POKEMONS=$(curl -sL https://pokeapi.co/api/v2/pokemon?limit=10000 | jq '.results[].name' -r)    
+for pokemon in $ALL_POKEMONS;
 do
   echo "Name of the Pokemon is: $pokemon"
 done
