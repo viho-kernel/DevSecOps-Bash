31c5da6ca007e408dcba0759692588251ae6c943
viho-kernel <vihari.reddy1802@gmail.com>
2025-12-23 17:24:06 +0530

    Second commit

--- nginx_status.sh
+++ nginx_status.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-cowsay "This is an example"
-
-systemctl status nginx
+systemctl status nginx >> /dev/null
 
 if [ $? -eq 0 ]; then
     echo "The previous command executed successfully. Nginx is running."
 else
     echo "The previous command failed. Nginx is not running."
 
-cowsay "Please check the nginx service."
-cowsay "Exiting the script now."
-cowsay "Ambeee"
 fi
