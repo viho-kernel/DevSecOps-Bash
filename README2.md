52eae343c8805251e49882779d6cbac3d24ee164
viho-kernel <vihari.reddy1802@gmail.com>
2025-12-23 16:32:58 +0530

    Second commit

--- README2.md
+++ README2.md
@@ -3,3 +3,25 @@
 
 if () --> start
 fi --> end
+
+=======================================================
+Day-3
+== 
+!=
+>=
+<=
+=================if-condition=================
+if [$a == $b]
+then
+echo "abcd"
+else
+echo "efgh"
+fi
+#if is written in brackets and always if statement must be closed with fi.
+
+if we are using <=, >= then we have to keep it in (()) ==> Double brackets
+if we are using -lt, -gt then we have to keep it in [] => single brackets
+
+#htop -->> will show utilization
+
+#Create a slack channgel and intergrate webhooks
--- find_memory_utilization.sh
+++ find_memory_utilization.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+SLACK_WEB='https://hooks.slack.com/services/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'
 
 TOTAL_MEM=$(free -m | grep -i mem | awk -F " " ' {print $2}')
 AVAILABLE_MEM=$(free -m | grep -i mem | awk -F " " ' {print $7}')
@@ -6,4 +7,12 @@ USED_MEM=$(expr $TOTAL_MEM - $AVAILABLE_MEM)
 echo "Total Memory (in MB): $TOTAL_MEM"
 echo "Used Memory (in MB): $USED_MEM"
 echo "Available Memory (in MB): $AVAILABLE_MEM"
-echo "Memory Utilization Percentage: $(( $USED_MEM * 100 / $TOTAL_MEM ))%"
+echo "Used Memory Utilization Percentage: $(( $USED_MEM * 100 / $TOTAL_MEM ))%"
+echo "Available Memory Utilization Percentage: $(( $AVAILABLE_MEM * 100 / $TOTAL_MEM ))%"
+
+if (($AVAILABLE_MEM <= 600)); then   
+    echo "Memory is critically low. Sending Slack notification..."
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Alert: Memory is critically low. Only $AVAILABLE_MEM MB available out of $TOTAL_MEM MB.\"}" $SLACK_WEB
+else
+    echo "Memory utilization is within acceptable limits."
+fi
