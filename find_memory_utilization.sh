6ed7dcb3806e32c9c59ae1e036b449a53d2d9ec0
viho-kernel <vihari.reddy1802@gmail.com>
2025-12-23 12:26:10 +0530

    Second commit

--- find_memory_utilization.sh
+++ find_memory_utilization.sh
@@ -6,4 +6,4 @@ USED_MEM=$(expr $TOTAL_MEM - $AVAILABLE_MEM)
 echo "Total Memory (in MB): $TOTAL_MEM"
 echo "Used Memory (in MB): $USED_MEM"
 echo "Available Memory (in MB): $AVAILABLE_MEM"
-echo "Memory Utilization Percentage: $(( $USED_MEM * 100 / $TOTAL_MEM ))%"
+echo "Memory Utilization Percentage: $(( $USED_MEM * 100 / $TOTAL_MEM | bc -1))%"
