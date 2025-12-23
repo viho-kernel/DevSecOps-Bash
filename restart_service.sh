1142d39b0000f08f76cc71b7933df33dfbd7c25b
viho-kernel <vihari.reddy1802@gmail.com>
2025-12-23 21:15:18 +0530

    Second commit

--- restart_service.sh
+++ restart_service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $# -gt 0 ]; then
-    for REGION in $@; do
+    for REGION in $1; do
         aws ec2 describe-vpcs --region $1 \
         | jq -r '.Vpcs[].VpcId'
         echo "---------------------"        
