3fad44fb1071a58e00797a3067a83ee4a649337c
viho-kernel <vihari.reddy1802@gmail.com>
2025-12-23 16:39:43 +0530

    Second commit

--- find_memory_utilization.sh
+++ find_memory_utilization.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ipofme=$(hostname -I | awk '{print $1}')
 SLACK_WEB='https://hooks.slack.com/services/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'
 
 TOTAL_MEM=$(free -m | grep -i mem | awk -F " " ' {print $2}')
@@ -13,7 +14,7 @@ echo "Used Memory Utilization Percentage: $USED_PERCENTAGE%"
 echo "Available Memory Utilization Percentage: $AVAILABLE_PERCENTAGE%"
 
 if (($AVAILABLE_PERCENTAGE <= 60)); then   
-    echo "Memory is critically low. Sending Slack notification..."
+    echo "Memory utilization of $ipofme is critically low. Sending Slack notification..."
     curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Alert: Memory is critically low. Only $AVAILABLE_MEM MB available out of $TOTAL_MEM MB.\"}" $SLACK_WEB
     curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Alert: Memory Utilization is at $USED_PERCENTAGE%. Please take necessary actions.\"}" $SLACK_WEB
     echo "Slack notification sent."
