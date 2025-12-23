3380334a47f475383bd6820ea83f80f7ee3c1780
viho-kernel <vihari.reddy1802@gmail.com>
2025-12-23 13:01:51 +0530

    Second commit

--- new_utilization.sh
+++ new_utilization.sh
@@ -2,12 +2,13 @@
 FREE_MEM=$(df -h | grep -i '/dev/root' | awk -F " " '{print $4}')
 USED_MEM=$(df -h | grep -i '/dev/root' | awk -F " " '{print $3}')
 TOTAL_MEM=$(df -h | grep -i '/dev/root' | awk -F " " '{print $2}')
+DISK_MEM_UTIL=$(df -h | grep -i '/dev/root' | awk -F " " '{print $5}' | tr -d '%')
 echo "Total Disk Memory: $TOTAL_MEM"
 echo "Used Disk Memory: $USED_MEM"
 echo "Free Disk Memory: $FREE_MEM"
-echo "Disk Memory Utilization Percentage: $(df -h | grep -i '/dev/root' | awk -F " " '{print $5}')"
+echo "Disk Memory Utilization Percentage: $DISK_MEM_UTIL"
 
-if $USED_MEM > 80%; then
+if [ $DISK_MEM_UTIL -gt 80 ]; then
   echo "Warning: Disk memory utilization is above 80%."
 else
   echo "Disk memory utilization is within acceptable limits."
