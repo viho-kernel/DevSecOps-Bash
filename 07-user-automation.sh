00c7545a2e83024f90dfcf0524a0771ba3e87bb6
viho-kernel <vihari.reddy1802@gmail.com>
2025-12-24 17:55:32 +0530

    latest commit

---
+++ 07-user-automation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#check how many parameters are passed if no parameters exit the script
+#check if user is already existed and create if not exists.
+#make sure user has sudo permissions.
+#Expire the password forcing to reset it.
+
+SLACK_WEB='https://hooks.slack.com/services/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'
+if [$# -gt 0]; then
+for USERNAME in $@; do
+#CHECK EXISTING USER
+EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
+if ["${USERNAME}" = "${EXISTING_USER}"]; then
+echo "The User ${USERNAME} Already Exists. Try a difference Username."
+else
+echo "Lets Create user ${USERNAME}."
+sudo useradd -m $USERNAME --shell /bin/bash -d /home/${USERNAME}
+sudo usermod -aG sudo ${USERNAME}
+echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers
+SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | head -1)
+PASSWORD="India@{RANDOM}${SPEC}"
+echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
+passwd -e ${USERNAME}
+echo "The Temporary Credentails are ${USERNAME} and ${PASSWORD}"
+curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Username is: ${USERNAME}\"}" >>/dev/null
+            curl -X POST ${SLACK_WEB} -sL -H 'Content-type: application/json' --data "{"text": \"Temporary Password Is: ${PASSWORD}  Reset This Password Immediatly.\"}" >>/dev/null
+fi
+done
+else
+echo "You have given $# Arguments. Please provide atleast one Arg.'
+fi
---
+++ README3.md
@@ -0,0 +1,24 @@
+==================DAY-7=======================
+User Creation Automation:
+Creating a user
+setting home directory
+giving a random password to it
+asking user to change it.
+
+/etc/passwd ==> to check users
+useradd -m vihari ==> to add user
+grep -i ==> to match relative words
+grep -w ==> to match exact word
+
+$# == no. of arguments.
+$@ == all arugments.
+$RANDOM == will generate a randome function.
+
+fold -w1 ==> will break a horizontal sequence to a vertical sequence.
+fold -w1 | shuf ==> if you do this it will shuffle the horizontal code randomly
+head -1 | will take top character
+tail -1 | will take bottom character.
+
+to replace a text
+Translate -tr == to change a spectific character
+Stream Editor -sed == to change whole sequence in a line. 
