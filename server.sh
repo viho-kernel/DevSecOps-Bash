bb7fa26e9f77862c8a9dd0c26001479caf8280f7
viho-kernel <vihari.reddy1802@gmail.com>
2025-12-24 15:36:10 +0530

    latest commit

--- server.sh
+++ server.sh
@@ -5,5 +5,5 @@ echo ${SERVERS[@]}
 for SERVER in ${SERVERS[@]};
 do
   echo "Testing connectivity to: $SERVER"
-  netcat -z -n -v $SERVER 20-30 2>&1 | grep succeeded
+  netcat -z -n -v -w3 $SERVER 22 2>&1 | grep succeeded > /tmp/server_test.txt
 done
