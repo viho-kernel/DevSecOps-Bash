35cb1da7115d2efa79203f8ff2eaf7a8a77591cf
viho-kernel <vihari.reddy1802@gmail.com>
2025-12-26 12:18:10 +0530

    user notification

--- 07-user-automation.sh
+++ 07-user-automation.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-SLACK_WB='https://hooks.slack.com/services/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'
+SLACK_WEB='https://hooks.slack.com/services/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'
 USERNAME=$1
-if [$# -gt 0]; then
+if [ $# -gt 0 ]; then
 EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
-if ["${USERNAME}" = "${EXISTING_USER}"]; then
+if [ "${USERNAME}" = "${EXISTING_USER}" ]; then
 echo "The user ${USERNAME} Already Exists. Try with a different Username."
 else
 echo "Lets Create User ${USERNAME}."
-sudo useradd -m $USERNAME --shell /bin/bash -d /home/${USERNAME}
-SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf)
+sudo useradd -m $USERNAME --shell /bin/bash -d "/home/${USERNAME}"
+SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | head -1)
 PASSWORD="India@${RANDOM}${SPEC}"
 echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
-passwd -e ${USERNAME}
+suo passwd -e ${USERNAME}
 echo "The Temporary credentials are ${USERNAME} and ${PASSWORD}"
 curl -X POST $SLACK_WEB -sL -H 'Content-type: application/json' --data "{\"text\":\"Username is: ${USERNAME}.\"}" >> /dev/null
 curl -X POST $SLACK_WEB -sL -H 'Content-type: application/json' --data "{\"text\":\"Temporary password is ${PASSWORD}. Please change you're Temporary passwod immediately.\"}" 
