231ff211308e586f75acc7f0f1254adb91210585
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-06 18:23:25 +0530

    latest commit

--- 08-Regex-Functions.sh
+++ 08-Regex-Functions.sh
@@ -12,7 +12,7 @@ echo " The user ${USERNAME} already existed. Kindly check and provider valid use
 else
 echo "Let's create a user ${USERNAME} "
 sudo useradd -m $USERNAME --shell /bin/bash -d "/home/${USERNAME}"
-sudo usermod -aG sudo ${USERNAME} 
+sudo usermod -aG sudo ${USERNAME} #giving user root credentials
 echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers #this set no password to user
 SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | head -1)
 PASSWORD=${"India@${RANDOM}${SPEC}"}
@@ -23,6 +23,8 @@ curl -X POST $SLACK_WEB -sL -H 'Content-type: application/json' --data "{\"text\
 curl -X POST $SLACK_WEB -sL -H 'Content-type: application/json' --data "{\"text\":\"Temporary password is ${PASSWORD}. Please change you're Temporary passwod immediately.\"}" 
 
 fi
+
+else
  echo "The username given is not correct. Give a username with 6 character length and it contains 3 digits and  3 letters"
 fi
 done
