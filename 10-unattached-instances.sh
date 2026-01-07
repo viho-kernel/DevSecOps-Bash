10ab0f50e7c177961735817cd5239eac6238ed6b
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-07 16:04:32 +0530

    latest commit

--- 10-unattached-instances.sh
+++ 10-unattached-instances.sh
@@ -5,19 +5,19 @@ delete_unattached_vols(){
 vols=$(aws ec2 describe-volumes --region us-east-1 | jq ".Volumes[].VolumeId" | tr -d '"')
 for vol in $vols
 do
-status=$(aws ec2 describe-volumes --volume-ids $vol | jq ".Volumes[].Attachments[].State" -r)
-instance=$(aws ec2 describe-volumes --volume-ids $vol | jq ".Volumes[].Attachments[].InstanceId")
+status=$(aws ec2 describe-volumes --volume-ids "$vol" | jq ".Volumes[].Attachments[].State" -r)
+instance=$(aws ec2 describe-volumes --volume-ids "$vol" | jq ".Volumes[].Attachments[].InstanceId")
 
 if [ "$status" = 'attached' ]; 
 then
     
     echo " $vol is in use by the EC2 {$instance}. Don't delete it"
     curl -s -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\" $vol is in use by the EC2 {$instance} . Don't delete it.\"}" $WEB_URL
+    --data "{\"text\":\"$vol is in use by the EC2 $instance. Don't delete it.\"}" "$WEB_URL"
 else
     echo " $vol is not in use. Hence, proceeding with deleting it. "
     curl -s -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\"$vol is not in use by the EC2 so deleting it\"}" $WEB_URL
+    --data "{\"text\":\"$vol is not in use by the EC2 so deleting it\"}" "$WEB_URL"
     aws ec2 delete-volume --volume-id $vol
 fi
 
