64f6fe6bd26d0b1b640ba073a82d72b274ab5dfb
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-07 13:17:06 +0530

    latest commit

--- 09-function.sh
+++ 09-function.sh
@@ -5,22 +5,19 @@ delete_vols(){
 vols=$(aws ec2 describe-volumes --region us-east-1 | jq ".Volumes[].VolumeId" | tr -d '"')
 for vol in $vols
 do 
-   size=$(aws ec2 describe-volumes --volume-ids vol-072bb485c0d4990dc | jq ".Volumes[].Size")
-
+   size=$(aws ec2 describe-volumes --volume-ids $vol | jq ".Volumes[].Size")
    if [ $size -gt 5 ]
    then
-
-      echo "The volume is a production volume don't delete it."
+      echo "The $vol volume is a production volume don't delete it."
       curl -s -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\"${vols} is a proudction environment will not delete it.\"}" $WEB_URL
+    --data "{\"text\":\"${vol} is a proudction environment will not delete it.\"}" $WEB_URL
 
    else
 
-      echo "The volume is not a production volume. Proceeding with termination."
-
+      echo "The $vol volume is not a production volume. Proceeding with termination."
       aws ec2 delete-volume --volume-id $vol
       curl -s -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\"${vols} has been terminated\"}" $WEB_URL
+    --data "{\"text\":\"${vol} has been terminated\"}" $WEB_URL
    fi 
 done
 
