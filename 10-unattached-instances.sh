1c4d8f30b51fee95c10970e470c15ff05e0e174a
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-07 16:07:21 +0530

    latest commit

--- 10-unattached-instances.sh
+++ 10-unattached-instances.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
-#declare a function
 WEB_URL="https://hooks.slack.com/services/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"
+
 delete_unattached_vols(){
-vols=$(aws ec2 describe-volumes --region us-east-1 | jq ".Volumes[].VolumeId" | tr -d '"')
-for vol in $vols
-do
-status=$(aws ec2 describe-volumes --volume-ids "$vol" | jq ".Volumes[].Attachments[].State" -r)
-instance=$(aws ec2 describe-volumes --volume-ids "$vol" | jq ".Volumes[].Attachments[].InstanceId")
+  vols=$(aws ec2 describe-volumes --region us-east-1 | jq -r ".Volumes[].VolumeId")
+  for vol in $vols; do
+    status=$(aws ec2 describe-volumes --volume-ids "$vol" | jq -r ".Volumes[].Attachments[].State")
+    instance=$(aws ec2 describe-volumes --volume-ids "$vol" | jq -r ".Volumes[].Attachments[].InstanceId")
 
-if [ "$status" = 'attached' ]; 
-then
-    
-    echo " $vol is in use by the EC2 {$instance}. Don't delete it"
-    curl -s -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\"$vol is in use by the EC2 $instance. Don't delete it.\"}" "$WEB_URL"
-else
-    echo " $vol is not in use. Hence, proceeding with deleting it. "
-    curl -s -X POST -H 'Content-type: application/json' \
-    --data "{\"text\":\"$vol is not in use by the EC2 so deleting it\"}" "$WEB_URL"
-    aws ec2 delete-volume --volume-id $vol
-fi
-
-done
-
+    if [ "$status" = "attached" ]; then
+      echo "$vol is in use by EC2 $instance. Don't delete it."
+      curl -s -X POST -H 'Content-type: application/json' \
+        --data "{\"text\":\"$vol is in use by the EC2 $instance. Don't delete it.🗃️\"}" "$WEB_URL"
+    else
+      echo "$vol is not in use. Hence, proceeding with deleting it."
+      curl -s -X POST -H 'Content-type: application/json' \
+        --data "{\"text\":\"$vol is not in use. Deleting it...🚮\"}" "$WEB_URL"
+      aws ec2 delete-volume --volume-id "$vol"
+    fi
+  done
 }
-
-#call function here
 
 delete_unattached_vols
