934140eb78e488fbcffac7b8b3968ba315737a74
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-07 13:31:25 +0530

    latest commit

--- 09-function.sh
+++ 09-function.sh
@@ -4,23 +4,23 @@ WEB_URL="https://hooks.slack.com/services/T0A6205T7UY/B0A6DQAVBQD/MV2ERb01kcY62s
 delete_vols(){
 vols=$(aws ec2 describe-volumes --region us-east-1 | jq ".Volumes[].VolumeId" | tr -d '"')
 for vol in $vols
-do 
-   size=$(aws ec2 describe-volumes --volume-ids $vol | jq ".Volumes[].Size")
-   if [[ "$size" -gt 5 ]] ;
-   then
-      echo "The ${vol} volume is a production volume don't delete it."
-      curl -X POST -sL -H 'Content-type: application/json' \
-    --data "{\"text\":\"${vol} is a proudction volume will not delete it.\"}" $WEB_URL
+do
+size=$(aws ec2 describe-volumes --volume-ids $vol | jq ".Volumes[].Size")
 
-   else
+if [ "$size" -gt 20 ]; 
+then
+    
+    echo " $vol is a production volume. Please don't delete it. "
+    curl -s -X POST -H 'Content-type: application/json' \
+    --data "{\"text\":\" $vol is a production volume. Please don't delete it.\"}" $WEB_URL
+else
+    echo " $vol is not a proudction volume. Hence, proceeding with deleting it. "
+    curl -s -X POST -H 'Content-type: application/json' \
+    --data "{\"text\":\"$MESSAGE\"}" $WEB_URL
+    aws ec2 delete-volume --volume-id $vol
+fi
 
-      echo "The ${vol} volume is not a production volume. Proceeding with termination."
-      aws ec2 delete-volume --volume-id $vol
-      curl -X POST -sL -H 'Content-type: application/json' \
-    --data "{\"text\":\"${vol} has been terminated\"}" $WEB_URL
-   fi 
 done
-
 
 }
 
