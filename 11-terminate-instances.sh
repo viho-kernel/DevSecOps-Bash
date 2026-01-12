6e7054d6f21a7f040858d87ebed4ad90d618760f
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-12 23:41:37 +0530

    latest commit

--- 11-terminate-instances.sh
+++ 11-terminate-instances.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#This script is to pull the ec2 instances in your region 'us-east-1' which are of t3.micro instance
+
 terminate_instances() {
   ids=$(aws ec2 describe-instances --region us-east-1 | jq -r '.Reservations[].Instances[] | select(.InstanceType=="t3.micro") | .InstanceId')
 
